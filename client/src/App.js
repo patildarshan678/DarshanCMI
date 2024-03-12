1341f013c49c77b04ceaae864cc4be09eb06b87c
patildarshan678 <patildarshan678@gmail.com>
2024-03-12 23:29:24 +0530

    1.Setup path for each compoents.

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
 import Login from './components/Login';
-
+import SignUp from './components/SignUp';
+import {Router, Route, Routes} from  'react-router-dom';
+import Home from './components/home/Home';
 function App() {
   return (
     <div className="App">
-      <Login/>
+      {/* <Login/> */}
+      
+        <Routes>
+        <Route Component={Home} path='/home'/>
+        <Route Component={Login} path='/login'/>
+        <Route Component={SignUp} path='/signup'/>
+        </Routes>
+      
+      
     </div>
   );
 }
