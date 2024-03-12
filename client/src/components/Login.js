7e542283b10dc429302685bb34550fe6f9343d1b
patildarshan678 <patildarshan678@gmail.com>
2024-03-12 22:52:38 +0530

    10Inital commit with login form for react js

---
+++ client/src/components/Login.js
@@ -0,0 +1,22 @@
+import './Login.css'
+import React from 'react'
+import Form from 'react-bootstrap/Form';
+function Login() {
+    return (
+        <div className='Login_container'>
+            <Form title='Login Form' className='LoginForm'>
+            <h2>Login Form</h2>
+                <Form.Group className="mb-3" controlId="formGroupEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" placeholder="Enter email" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formGroupPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="Password" />
+                </Form.Group>
+            </Form>
+        </div>
+    )
+}
+
+export default Login
