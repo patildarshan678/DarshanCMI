f3039fdad17e0b272c009042741cc82109a61b31
patildarshan678 <patildarshan678@gmail.com>
2024-03-12 23:01:20 +0530

    Updatd the Login form.

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -5,19 +5,27 @@ import {Form,Button} from 'react-bootstrap';
 function Login() {
     return (
         <div className='Login_container'>
-            <Form title='Login Form' className='LoginForm'>
-            <h2>Login Form</h2>
-                <Form.Group className="mb-3" controlId="formGroupEmail">
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="formGroupPassword">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
-                </Form.Group>
-                
-            <Button>Login</Button>
-            </Form>
+           <Form>
+            <h2>Login From</h2>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Text className="text-muted">
+          We'll never share your email with anyone else.
+        </Form.Text>
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicCheckbox">
+        <Form.Check type="checkbox" label="Check me out" />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
         </div>
     )
 }
