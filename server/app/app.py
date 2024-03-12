2508c4173d9ae13d4b75827e24f9762b7edbabcd
patildarshan678 <patildarshan678@gmail.com>
2024-03-13 00:06:25 +0530

    Made flask app CORS Compatibel

--- server/app/app.py
+++ server/app/app.py
@@ -1,12 +1,14 @@
 from flask import Flask
 from database import db
 from Models.userModel import User
+from flask_cors import CORS
 from Models.csvModels import CSVData
 from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required
 
 flask_app = Flask(__name__)
 environment_configuration = 'config.Developement'
 flask_app.config.from_object(environment_configuration)
+CORS(flask_app)
 db.init_app(flask_app)
 login_manager = LoginManager()
 login_manager.init_app(flask_app)
